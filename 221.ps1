5292fa908733976ec3982eb735a40dba5ce98397
Abhay Parashar <52857736+Abhayparashar31@users.noreply.github.com>
2025-05-16 20:36:36 +0530

    221.ps1

--- 221.ps1
+++ 221.ps1
@@ -1,7 +1,10 @@
-# Step 1: Download CSV
+# Download CSV
 $csvUrl = "https://raw.githubusercontent.com/Abhayparashar31/datasets/refs/heads/master/Data.csv"
 $outputPath = "$env:USERPROFILE\Downloads\Data.csv"
 Invoke-WebRequest -Uri $csvUrl -OutFile $outputPath
 
-# Step 2: Launch Calculator
+# Open calculator
 Start-Process "calc.exe"
+
+# Drop a marker to verify execution
+"Script ran at $(Get-Date)" | Out-File "$env:USERPROFILE\Downloads\Executed_Marker.txt"
